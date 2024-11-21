1879f96293904ebc889af8fa9b1d2283fb9808cf
Isabela de Vito <80789978+isabelafdevito@users.noreply.github.com>
2024-11-21 14:07:23 -0300

    Update exercicio1.c

--- exercicio1.c
+++ exercicio1.c
@@ -1,13 +1,13 @@
 int main(){
 
-    int admin = 0;
+    int admin = 1;
     char buf[0x10];
 
     puts("PLease go ahead and leave a comment :");
     gets(buf);
 
-    if (admin){
-     puts("Hello admin!");
+    if (!admin){
+     puts("Hello NOT admin!");
     }
 
     else{
@@ -15,4 +15,5 @@ int main(){
         exit(1);
     }
 }
+
 
