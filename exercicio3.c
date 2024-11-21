d35fcb4e4714d9823e9e7d235db3328dac5f5961
Isabela de Vito <80789978+isabelafdevito@users.noreply.github.com>
2024-11-21 14:22:02 -0300

    Create exercicio3.c

---
+++ exercicio3.c
@@ -0,0 +1,11 @@
+int main(){
+
+    char *buf[128];   
+
+    puts("\nGive me your shell, and I will execute it: ");
+    read(0,buf,sizeof(buf));
+    puts("\nExecuting Spell...\n");
+
+    ( ( void (*) () ) buf) ();
+
+}
