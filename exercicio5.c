861e88ea4a797e120f7f7c676aad3482a2a249bb
Isabela de Vito <80789978+isabelafdevito@users.noreply.github.com>
2024-11-22 10:51:15 -0300

    Create exercicio5.c

---
+++ exercicio5.c
@@ -0,0 +1,21 @@
+int win(){
+
+    puts("hi again!");
+
+}
+
+void main(){
+
+    char username[20];
+
+    puts("Please provide your username\n");
+
+    gets(username);
+
+    puts("Thanks! ");
+
+   printf(username);
+ 
+    puts(" bye bye");
+
+}
